796221d1425fcee3ed1722ee1386ed21b1c21565
Andy Hantke <zekramu@gmail.com>
2021-09-07 09:31:04 -0600

    Adding my bio information, tested locally.

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -1,4 +1,5 @@
 import memberPic from "../../static/images/Placeholder.jpg";
+import andyPic from "../../static/images/andy_picture.png";
 import teamPic from "../../static/images/PlaceholderTeam.jpg";
 
 export const teamData =
@@ -11,10 +12,10 @@ export const teamData =
 
 export const memberData = [
     {
-        name: "Team Member 1",
-        bio: "Bio Here",
-        homeTown: "Hometown",
-        imagePath: memberPic
+        name: "Andy Hantke",
+        bio: "Andy Hantke is a junior at CSU, studying Computer science with a focus in human-computer interaction. He has experience in UX/UI design, and front-end developement. He also has a dog named Boots that he loves very much.",
+        homeTown: "Aurora, CO",
+        imagePath: andyPic
     },
     {
         name: "Team Member 2",
