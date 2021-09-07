c363268a0ef377170f693a1acf2900da32b1947d
Andy Hantke <38579990+ahantke1@users.noreply.github.com>
2021-09-07 09:06:48 -0700

    Added team name to client

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -16,7 +16,7 @@ setLogLevelIfDefault();
 
 export const LOG = ulog("App");
 
-export const CLIENT_TEAM_NAME = "t13 team name";
+export const CLIENT_TEAM_NAME = "t13 Penguinz";
 
 export const EARTH_RADIUS_UNITS_DEFAULT = { "miles": 3959 };
 
