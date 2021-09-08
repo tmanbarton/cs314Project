1cb08cd5ad89df3561c471d2abafc57e6973004b
Tyler Barton <tmanbarton@gmail.com>
2021-09-08 16:45:26 -0600

    Fix capitalization

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -13,19 +13,19 @@ export const teamData = {
 export const memberData = [
 	{
 		name: "Andy Hantke",
-		bio: "Andy Hantke is a Junior at CSU, studying Computer Science with a focus in human-computer interaction. He has experience in UX/UI design, and front-end developement. He also has a dog named Boots that he loves very much.",
+		bio: "Andy Hantke is a junior at CSU, studying computer science with a focus in human-computer interaction. He has experience in UX/UI design, and front-end developement. He also has a dog named Boots that he loves very much.",
 		homeTown: "Aurora, CO",
 		imagePath: andyPic,
 	},
 	{
 		name: "Amir Raissi",
-		bio: "Amir Raissi is a Junior at CSU, studying Computer Science with a focus in software engineering, as well as minoring in economics. Amir currently works remotely as a Software Engineer Intern. In his free time you can see him on the basketball courts or with his friends.",
+		bio: "Amir Raissi is a junior at CSU, studying computer science with a focus in software engineering, as well as minoring in economics. Amir currently works remotely as a Software Engineer Intern. In his free time you can see him on the basketball courts or with his friends.",
 		homeTown: "Littleton, CO",
 		imagePath: amirPic,
 	},
 	{
 		name: "Tyler Barton",
-		bio: "Tyler is a senior at CSU, studying Computer Science. He has experience in front-end development and an interest in back-end development. Tyler enjoys coding personal projects when he doesn't have homework and has a passion for Rubik's Cubes.",
+		bio: "Tyler is a senior at CSU, studying computer science. He has experience in front-end development and an interest in back-end development. Tyler enjoys coding personal projects when he doesn't have homework and has a passion for Rubik's Cubes.",
 		homeTown: "Parker, CO",
 		imagePath: tylerPic,
 	},
