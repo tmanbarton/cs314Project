e8950fd5189645ce6f69fb50dca055aa88888376
Rick Newby <richard.newby@bongolearn.com>
2021-09-20 08:26:30 -0600

    Fix FindRequest formatting

--- server/src/main/java/com/tco/requests/findRequest/FindRequest.java
+++ server/src/main/java/com/tco/requests/findRequest/FindRequest.java
@@ -11,12 +11,21 @@ public class FindRequest extends Request{
     private int found;
     private final transient Logger log = LoggerFactory.getLogger(FindRequest.class);
 
-    public FindRequest(){
+    /**
+     * Constructor
+     * Sets the request type to 'find'.
+     */
+    public FindRequest() {
         this.requestType = "find";
     }
 
+    /**
+     * Builds a Find Request response.
+     * This method builds a Find Request response using the match string and limit
+     * provided by the client in the Find Request.
+     */
     @Override
-    public void buildResponse(){
+    public void buildResponse() {
         // DBQuery db = new DBQuery(match, limit);
         places = new ArrayList<>();
         // places = db.findByString();
@@ -24,21 +33,47 @@ public class FindRequest extends Request{
         log.trace("buildResponse -> {}", this);
     }
 
+    /**
+     * Returns the Find Request match string.
+     * The match is a string used to search for places within the database.
+     * @return The match
+     */
     public String getMatch() {
         return this.match;
     }
 
+    /**
+     * Sets the match string used in the Find Request.
+     * The match string is used to search for places within the database.
+     * @param The Find Request match
+     */
     public void setMatch(String match) {
         this.match = match;
     }
 
-    public int getLimit() { return this.limit; }
+    /**
+     * Returns the limit used in the Find Request.
+     * The limit is the maximum number of places returned in a Find Request.
+     * @return The Find request limit
+     */
+    public int getLimit() {
+        return this.limit;
+    }
 
-    public void setLimit(int limit) { this.limit = limit; }
+    /**
+     * Sets the limit used in the Find Request.
+     * The limit is the maximum number of places returned in a Find Request.
+     * @param limit
+     */
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
 
-    public int getFound() { return this.found; }
-
-    public void setFound(int limit) { this.found = found; }
-
-
+    /**
+     * Returns the number of places found in the Find Request.
+     * @return The number of places found
+     */
+    public int getFound() {
+        return this.found;
+    }
 }
