d69dc0fbdc71dea9a0cb513b6d40bb0ce1e9fd6b
amir-raissi <amir_raissi@yahoo.com>
2021-09-22 16:41:21 -0600

    Added SearchInput component. More testing rquired.

--- client/src/components/Trip/Search/SearchInput.js
+++ client/src/components/Trip/Search/SearchInput.js
@@ -1,19 +1,53 @@
-import React from 'react'
+import { React, useState } from "react";
+import { Input } from "reactstrap";
+import { sendAPIRequest, getOriginalServerUrl } from "../../../utils/restfulAPI";
+import { SearchResults } from "./SearchResults";
 
+const [match, setMatch] = useState("");
+const [places, setPlaces] = useState({});
+const limit = 10;
+const serverUrl = getOriginalServerUrl();
 
-export default function SearchInput(props) {
-    // return (
-
-    // );
+export default function SearchInput() {
+	return (
+		<Container>
+			<Section>
+				<Input
+					type="text"
+					placeholder="Search for Places"
+					onChange={inputChanged}
+				></Input>
+			</Section>
+			<Section>
+				<SearchResults places={places} />
+			</Section>
+		</Container>
+	);
 }
 
+function inputChanged(input) {
+	setMatch(input);
+	buildRequest();
+	sendFindRequest();
+}
 
-async function sendFindRequest() {
-	const findResponse = await sendAPIRequest({ requestType: "find" }, serverUrl);
+function buildRequest() {
+	let request = {
+		requestType: "find",
+		match: match,
+		limit: limit,
+	};
+	sendFindRequest(request);
+}
+
+async function sendFindRequest(request) {
+	const findResponse = await sendAPIRequest(request, serverUrl);
 	if (findResponse) {
-		const places = findResponse["properties"].places;
+		setPlaces(findResponse["properties"].places);
 	} else {
-		showMessage(`Find request to ${serverUrl} failed. Check the log for more details.`, "error");
+		showMessage(
+			`Find request to ${serverUrl} failed. Check the log for more details.`,
+			"error"
+		);
 	}
-	return places;
 }
