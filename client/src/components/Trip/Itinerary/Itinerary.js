11afc7a7464347f6cbd6d8588f30cdcc0c9ac193
Tyler Barton <tmanbarton@gmail.com>
2021-09-22 21:27:34 +0000

    Toggle search bar on clicking search icon

--- client/src/components/Trip/Itinerary/Itinerary.js
+++ client/src/components/Trip/Itinerary/Itinerary.js
@@ -1,40 +1,54 @@
 import React from 'react';
-import { Table, ButtonGroup, Container, Row, Col } from 'reactstrap';
-import { PlaceActionsDropdown } from './actions.js';
+import { Input, Table, Collapse, Container } from 'reactstrap';
 import { latLngToText } from '../../../utils/transformers';
 import { FaHome, FaTrashAlt, FaSearch } from 'react-icons/fa';
 import { DEFAULT_STARTING_PLACE } from '../../../utils/constants';
+import { useToggle } from '../../../hooks/useToggle';
 
 export default function Itinerary(props) {
     return (
-        <Container>
+        <Table responsive striped>
             <Header placeActions={props.placeActions} />
-            <hr />
-            <Table responsive striped>
-                <Body places={props.places} placeActions={props.placeActions} />
-            </Table>
-        </Container>
-        
+            <Body places={props.places} placeActions={props.placeActions} />
+        </Table>
     );
 }
 
 function Header(props) {
+    const [showSearch, toggleSearch] = useToggle(false);
+    
     return (
-        <Row>
-            <Col>
-                <h4>My Trip</h4>
-            </Col>
-            <Col>
-                <div className="float-right"> 
-                    <FaHome size={24} onClick={() => props.placeActions.append(DEFAULT_STARTING_PLACE)} data-testid='home-button'></FaHome>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <FaSearch size={24} />
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <FaTrashAlt size={24} onClick={() => props.placeActions.removeAll()} data-testid='delete-all-button'></FaTrashAlt>
-                </div>
-            </Col>
-        </Row>
+        <thead>
+            <tr>
+                <th/>
+                <th>My Trip</th>
+                <th>
+                    <FaHome onClick={() => props.placeActions.append(DEFAULT_STARTING_PLACE)} data-testid='home-button'/>
+                </th>    
+                <th>
+                    <Search toggleSearch={toggleSearch}/>
+                    <Container>
+                        <Collapse isOpen={showSearch}>
+                            <Input />
+                        </Collapse>
+                    </Container>
+                </th>
+                <th>
+                    <FaTrashAlt onClick={() => props.placeActions.removeAll()} data-testid='delete-all-button'/>
+                </th>
+            </tr>
+            <tr>
+                {/* <Input/> */}
+            </tr>
+        </thead>
+    );
+}
 
+function Search(props) {
+    return (
+        <>
+            <FaSearch onClick={props.toggleSearch}/>
+        </>
     );
 }
 
@@ -66,7 +80,7 @@ function TableRow(props) {
                 <small className="text-muted">{location}</small>
             </td>
             <td>
-                <PlaceActionsDropdown placeActions={props.placeActions} index={props.index} />
+                <FaTrashAlt onClick={() => props.placeActions.removeAtIndex(props.index)} data-testid={`delete-button-${props.index}`}/>
             </td>
         </tr>
     );
