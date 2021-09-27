b3300e5a031120662e5ba7993ef28ac5a88eafdd
amir-raissi <amir_raissi@yahoo.com>
2021-09-27 16:34:27 -0600

    Added data structure to populate drop down items

---
+++ client/src/components/Margins/avalibleServers.js
@@ -0,0 +1,118 @@
+export const avaliableServers = [
+	{
+		teamNumber: "t00",
+		extension: "31400",
+	},
+	{
+		teamNumber: "t01",
+		extension: "31401",
+	},
+	{
+		teamNumer: "t02",
+		extension: "31402",
+	},
+	{
+		teamNumer: "t03",
+		extension: "31403",
+	},
+	{
+		teamNumer: "t04",
+		extension: "31404",
+	},
+	{
+		teamNumer: "t05",
+		extension: "31405",
+	},
+	{
+		teamNumer: "t06",
+		extension: "31406",
+	},
+	{
+		teamNumer: "t07",
+		extension: "31407",
+	},
+	{
+		teamNumer: "t08",
+		extension: "31408",
+	},
+	{
+		teamNumer: "t09",
+		extension: "31409",
+	},
+	{
+		teamNumer: "t10",
+		extension: "31410",
+	},
+	{
+		teamNumer: "t11",
+		extension: "31411",
+	},
+	{
+		teamNumer: "t12",
+		extension: "31412",
+	},
+	{
+		teamNumer: "t13",
+		extension: "31413",
+	},
+	{
+		teamNumer: "t14",
+		extension: "31414",
+	},
+	{
+		teamNumer: "t15",
+		extension: "31415",
+	},
+	{
+		teamNumer: "t16",
+		extension: "31416",
+	},
+	{
+		teamNumer: "t17",
+		extension: "31417",
+	},
+	{
+		teamNumer: "t18",
+		extension: "31418",
+	},
+	{
+		teamNumer: "t19",
+		extension: "31419",
+	},
+	{
+		teamNumer: "t20",
+		extension: "31420",
+	},
+	{
+		teamNumer: "t21",
+		extension: "31421",
+	},
+	{
+		teamNumer: "t22",
+		extension: "31422",
+	},
+	{
+		teamNumer: "t23",
+		extension: "31423",
+	},
+	{
+		teamNumer: "t24",
+		extension: "31424",
+	},
+	{
+		teamNumer: "t25",
+		extension: "31425",
+	},
+	{
+		teamNumer: "t26",
+		extension: "31426",
+	},
+	{
+		teamNumer: "t27",
+		extension: "31427",
+	},
+	{
+		teamNumer: "t99",
+		extension: "31499",
+	},
+];
