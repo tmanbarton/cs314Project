51b3e255e8659f701a8015a997777a04d136458a
amir-raissi <amir_raissi@yahoo.com>
2021-09-27 16:58:44 -0600

    fixed typo

--- client/src/components/Margins/avalibleServers.js
+++ client/src/components/Margins/avalibleServers.js
@@ -1,118 +1,118 @@
 export const avaliableServers = [
 	{
-		teamNumber: "t00 Double Aughts",
+		teamName: "t00 Double Aughts",
 		extension: "31400",
 	},
 	{
-		teamNumber: "t01 TRNG",
+		teamName: "t01 TRNG",
 		extension: "31401",
 	},
 	{
-		teamNumer: "t02 Git Good",
+		teamName: "t02 Git Good",
 		extension: "31402",
 	},
 	{
-		teamNumer: "t03 Indecisive Individuals",
+		teamName: "t03 Indecisive Individuals",
 		extension: "31403",
 	},
 	{
-		teamNumer: "t04 Geek Velocity",
+		teamName: "t04 Geek Velocity",
 		extension: "31404",
 	},
 	{
-		teamNumer: "t05 Tabsoverspaces",
+		teamName: "t05 Tabsoverspaces",
 		extension: "31405",
 	},
 	{
-		teamNumer: "t06 Here For Now",
+		teamName: "t06 Here For Now",
 		extension: "31406",
 	},
 	{
-		teamNumer: "t07 007",
+		teamName: "t07 007",
 		extension: "31407",
 	},
 	{
-		teamNumer: "t08 Ambiguous Aardvarks",
+		teamName: "t08 Ambiguous Aardvarks",
 		extension: "31408",
 	},
 	{
-		teamNumer: "t09 Coding Crew",
+		teamName: "t09 Coding Crew",
 		extension: "31409",
 	},
 	{
-		teamNumer: "t10 Brunch Boys",
+		teamName: "t10 Brunch Boys",
 		extension: "31410",
 	},
 	{
-		teamNumer: "t11 The Brewsters",
+		teamName: "t11 The Brewsters",
 		extension: "31411",
 	},
 	{
-		teamNumer: "t12 Dodeca-Rams",
+		teamName: "t12 Dodeca-Rams",
 		extension: "31412",
 	},
 	{
-		teamNumer: "t13 Penguinz",
+		teamName: "t13 Penguinz",
 		extension: "31413",
 	},
 	{
-		teamNumer: "t14 The Kahunas",
+		teamName: "t14 The Kahunas",
 		extension: "31414",
 	},
 	{
-		teamNumer: "t15 Golden Potatoes",
+		teamName: "t15 Golden Potatoes",
 		extension: "31415",
 	},
 	{
-		teamNumer: "t16 The Sixteens",
+		teamName: "t16 The Sixteens",
 		extension: "31416",
 	},
 	{
-		teamNumer: "t17 We Push To Main",
+		teamName: "t17 We Push To Main",
 		extension: "31417",
 	},
 	{
-		teamNumer: "t18 Break Main",
+		teamName: "t18 Break Main",
 		extension: "31418",
 	},
 	{
-		teamNumer: "t19 The Space Between",
+		teamName: "t19 The Space Between",
 		extension: "31419",
 	},
 	{
-		teamNumer: "t20 Squinting in the Back",
+		teamName: "t20 Squinting in the Back",
 		extension: "31420",
 	},
 	{
-		teamNumer: "t21 Ctrl Alt Elite",
+		teamName: "t21 Ctrl Alt Elite",
 		extension: "31421",
 	},
 	{
-		teamNumer: "t22 2 Eazy",
+		teamName: "t22 2 Eazy",
 		extension: "31422",
 	},
 	{
-		teamNumer: "t23 Stuck in Beta",
+		teamName: "t23 Stuck in Beta",
 		extension: "31423",
 	},
 	{
-		teamNumer: "t24 24 Karat",
+		teamName: "t24 24 Karat",
 		extension: "31424",
 	},
 	{
-		teamNumer: "t25 Boat Smarts",
+		teamName: "t25 Boat Smarts",
 		extension: "31425",
 	},
 	{
-		teamNumer: "t26 Hamiltonian Cycle",
+		teamName: "t26 Hamiltonian Cycle",
 		extension: "31426",
 	},
 	{
-		teamNumer: "t27 Codey Crew",
+		teamName: "t27 Codey Crew",
 		extension: "31427",
 	},
 	{
-		teamNumer: "t99 team name",
+		teamName: "t99 team name",
 		extension: "31499",
 	},
 ];
