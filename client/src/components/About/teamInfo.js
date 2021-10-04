4974a6fcdb26660f3a91f740c76e5abcbdfc4222
amir-raissi <amir_raissi@yahoo.com>
2021-10-04 17:45:22 -0600

    Updated our team info since Richard is no longer a part of the team

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -2,7 +2,6 @@ import memberPic from "../../static/images/Placeholder.jpg";
 import andyPic from "../../static/images/andy_picture.png";
 import amirPic from "../../static/images/amir_picture.png";
 import tylerPic from "../../static/images/tyler_picture.png";
-import rickPic from "../../static/images/rick_picture.png";
 import teamPic from "../../static/images/teamPicWebsite.png";
 import billPic from "../../static/images/bill_picture.png";
 
@@ -30,12 +29,6 @@ export const memberData = [
 		bio: "Tyler is a senior at CSU, studying computer science. He has experience in front-end development and an interest in back-end development. Tyler enjoys coding personal projects when he doesn't have homework and has a passion for Rubik's Cubes.",
 		homeTown: "Parker, CO",
 		imagePath: tylerPic,
-	},
-	{
-		name: "Richard Newby",
-		bio: "Richard is a an online student at CSU, studying computer science with a focus in software engineering, though has a background in electrical engineering and is pursuing a second bachelors degree. Richard currently works as a software engineer for an educational technology company and enjoys playing guitar when not working.",
-		homeTown: "San Diego, CA",
-		imagePath: rickPic,
 	},
 	{
 		name: "William (Bill) Price",
