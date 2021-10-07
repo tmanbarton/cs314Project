4f49b04ea8e2d0445e4300007449f39a0486a6c6
amir-raissi <amir_raissi@yahoo.com>
2021-10-07 15:34:14 -0600

    Initial commit

---
+++ server/src/test/java/com/tco/requests/TestDistancesRequest.java
@@ -0,0 +1,12 @@
+package com.tco.requests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestDistancesRequest {
+    
+}
