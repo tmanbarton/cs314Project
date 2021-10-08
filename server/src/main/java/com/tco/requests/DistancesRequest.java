4ba1cb4e4ea529f2522b3e9694b717f74a3201cd
amir-raissi <amir_raissi@yahoo.com>
2021-10-08 17:10:18 -0600

    Add DistancesRequestTest and fixed bugs in DistanceRequest as they came up.

--- server/src/main/java/com/tco/requests/DistancesRequest.java
+++ server/src/main/java/com/tco/requests/DistancesRequest.java
@@ -1,63 +1,81 @@
 package com.tco.requests;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.lang.Math;
+import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DistancesRequest extends Request {
-    private double earthRadius;
-    private ArrayList<Integer> distances = new ArrayList<Integer>();
-    private Places places;
+  private double earthRadius;
+  private ArrayList<Integer> distances = new ArrayList<Integer>();
+  private Places places;
 
-    private final transient Logger log = LoggerFactory.getLogger(DistancesRequest.class);
+  private final transient Logger log =
+      LoggerFactory.getLogger(DistancesRequest.class);
 
-    @Override
-    public void buildResponse() {
-        distances = getDistances(places);
+  @Override
+  public void buildResponse() {
+    distances = getDistances(places);
 
-        log.trace("buildResponse -> {}", this);
+    log.trace("buildResponse -> {}", this);
+  }
+
+  public ArrayList<Integer> getDistances(Places places) {
+    ArrayList<Integer> distances = new ArrayList<Integer>();
+    double latitude1 = 0;
+    double longitude1 = 0;
+    double latitude2 = 0;
+    double longitude2 = 0;
+    for (int i = 1; i < places.size(); i++) {
+      latitude1 = Double.parseDouble(places.get(i - 1).get("latitude"));
+      longitude1 = Double.parseDouble(places.get(i - 1).get("longitude"));
+
+      latitude2 = Double.parseDouble(places.get(i).get("latitude"));
+      longitude2 = Double.parseDouble(places.get(i).get("longitude"));
+
+      int distance =
+          computeDistance(latitude1, latitude2, longitude1, longitude2);
+      distances.add(distance);
     }
+    latitude1 =
+        Double.parseDouble(places.get(places.size() - 1).get("latitude"));
+    longitude1 =
+        Double.parseDouble(places.get(places.size() - 1).get("longitude"));
 
-    public ArrayList<Integer> getDistances(Places places) {
-        ArrayList<Integer> distances = new ArrayList<Integer>();
-        double latitude1 = 0;
-        double longitude1 = 0;
-        double latitude2 = 0;
-        double longitude2 = 0;
-        for(int i = 1; i <= places.size(); i++) {
-            latitude1 = Double.parseDouble(places.get(i - 1).get("latitude"));
-            longitude1 = Double.parseDouble(places.get(i - 1).get("longitude"));
-            
-            if(i == places.size()) {
-                latitude2 = Double.parseDouble(places.get(0).get("latitude"));
-                longitude2 = Double.parseDouble(places.get(0).get("longitude"));
-            }
-            else {
-                latitude2 = Double.parseDouble(places.get(i).get("latitude"));
-                longitude2 = Double.parseDouble(places.get(i).get("longitude"));
-            }
-            int distance = computeDistance(latitude1, latitude2, longitude1, longitude2);
-            distances.add(distance);
-        }
-        return distances;
-    }
+    latitude2 = Double.parseDouble(places.get(0).get("latitude"));
+    longitude2 = Double.parseDouble(places.get(0).get("longitude"));
 
-    public DistancesRequest() {
-        this.requestType = "distances";
-    }
+    int distance =
+        computeDistance(latitude1, latitude2, longitude1, longitude2);
+    distances.add(distance);
 
-    public int computeDistance(double latitude1, double latitude2, double longitude1, double longitude2) {
-        latitude1 = Math.toRadians(latitude1);
-        latitude2 = Math.toRadians(latitude2);
-        longitude1 = Math.toRadians(longitude1);
-        longitude2 = Math.toRadians(longitude2);
-//        distance = 2 * radius * arcsin(sqrt(sin^2((lat2 - lat1)/2) + cos(lat1)*cos(lat2)*sin^2((long2 - long1)/2)))
-        double distance = 2 * this.earthRadius * Math.asin(Math.sqrt( Math.pow(Math.sin( (latitude2 - latitude1)/2 ), 2) + Math.cos(latitude1) * Math.cos(latitude2) * Math.pow(Math.sin( (longitude2-longitude1)/2 ) , 2) ));
-        return (int)distance;
-    }
-    
+    return distances;
+  }
+
+  public DistancesRequest() { this.requestType = "distances"; }
+
+  public int computeDistance(double latitude1, double latitude2,
+                             double longitude1, double longitude2) {
+    latitude1 = Math.toRadians(latitude1);
+    latitude2 = Math.toRadians(latitude2);
+    longitude1 = Math.toRadians(longitude1);
+    longitude2 = Math.toRadians(longitude2);
+    double distance =
+        2 * (this.earthRadius * 1.609344) * //this is a temp fix converting the earthRadius to km
+        Math.asin(Math.sqrt(
+            Math.pow(Math.sin((latitude2 - latitude1) / 2), 2) +
+            Math.cos(latitude1) * Math.cos(latitude2) *
+                Math.pow(Math.sin((longitude2 - longitude1) / 2), 2)));
+    distance *= .6214; //this is a temp fix converting the distance to miles
+    return (int)distance;
+  }
+
+  public void setEarthRadius(double earthRadius){
+      this.earthRadius = earthRadius;
+  }
+  public double getEarthRadius(){
+      return earthRadius;
+  }
 }
--- server/src/test/java/com/tco/requests/TestDistancesRequest.java
+++ server/src/test/java/com/tco/requests/TestDistancesRequest.java
@@ -3,10 +3,57 @@ package com.tco.requests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestDistancesRequest {
-    
+  private ArrayList<String> jsonStrings = new ArrayList<String>(Arrays.asList(
+      "{\"latitude\": \"34.305599212646484\", \"longitude\": \"-112.16500091552734\"}",
+      "{\"latitude\": \"35.350498199499995\",\"longitude\": \"-116.888000488\"}",
+      "{\"latitude\": \"28.64550018310547\",\"longitude\": \"-82.21900177001953\"}",
+      "{\"latitude\": \"27.230899810791016\",\"longitude\": \"-80.96920013427734\"}",
+      "{\"latitude\": \"33.76750183105469\",\"longitude\": \"-84.06829833984375\"}",
+      "{\"latitude\": \"48.145301818847656\",\"longitude\": \"-116.21399688720703\"}"));
+
+  private ArrayList<Integer> correctDistances =
+      new ArrayList<Integer>(Arrays.asList(277, 2072, 123, 487, 1928, 978));
+
+  private DistancesRequest distances;
+  Places places = new Places();
+  private double earthRadius = 3958.8;
+
+  @BeforeEach
+  public void createConfigurationForTestCases() {
+    distances = new DistancesRequest();
+    distances.setEarthRadius(earthRadius);
+    for (int i = 0; i < jsonStrings.size(); i++) {
+      Place place = new Gson().fromJson(jsonStrings.get(i), Place.class);
+      places.add(place);
+    }
+  }
+
+  @Test
+  @DisplayName("Request type is \"distances\"")
+  public void testType() {
+    String type = distances.getRequestType();
+    assertEquals("distances", type);
+  }
+
+  @Test
+  @DisplayName("Earth Radius is correct")
+  public void testEarthRadius() {
+    double eR = distances.getEarthRadius();
+    assertEquals(eR, earthRadius);
+  }
+
+  @Test
+  @DisplayName("Distances is correct")
+  public void testDistances() {
+    ArrayList<Integer> distancesArr = distances.getDistances(places);
+    assertEquals(distancesArr, correctDistances);
+  }
 }
