bae0e06a054d0cd5b13efe2ce53ae832908b2843
amir-raissi <amir_raissi@yahoo.com>
2021-10-10 20:15:05 -0600

    refactor footer

--- client/src/components/Margins/Footer.js
+++ client/src/components/Margins/Footer.js
@@ -20,31 +20,38 @@ const supportedFeatures = ["config", "find"];
 export default function Footer(props) {
 	const [featuresChecklistOpen, toggleFeaturesChecklist] = useToggle(false);
 	const [featuresRecieved, setFeaturesRecieved] = useState(["config", "find"]);
+	let stateMethods = {
+		toggleFeaturesChecklist: toggleFeaturesChecklist,
+		setFeaturesRecieved: setFeaturesRecieved,
+		setDisableSearch: props.setDisableSearch,
+		processServerConfigSuccess: props.processServerConfigSuccess,
+	};
+	let stateVariables = {
+		featuresChecklistOpen: featuresChecklistOpen,
+		featuresRecieved: featuresRecieved,
+		disableSearch: props.disableSearch,
+	};
 
 	return (
 		<div className="full-width footer">
 			<ServerInformation
-				toggleFeaturesChecklist={toggleFeaturesChecklist}
-				featuresChecklistOpen={featuresChecklistOpen}
 				showMessage={props.showMessage}
-				featuresRecieved={featuresRecieved}
-				setFeaturesRecieved={setFeaturesRecieved}
-				setDisableSearch={props.setDisableSearch}
-				disableSearch={props.disableSearch}
+				stateMethods={stateMethods}
+				stateVariables={stateVariables}
 				{...props}
 			/>
 		</div>
 	);
 }
 
-function evaluateFeatures(avaliableFeatures, stateMethods, states) {
+function evaluateFeatures(avaliableFeatures, stateMethods, stateVariables) {
 	for (let i = 0; i < supportedFeatures.length; i++) {
 		if (supportedFeatures[i] == "find") {
 			if (
 				(avaliableFeatures.indexOf(supportedFeatures[i]) == -1 &&
-					!states.disableSearch) ||
+					!stateVariables.disableSearch) ||
 				(avaliableFeatures.indexOf(supportedFeatures[i]) > -1 &&
-					states.disableSearch)
+					stateVariables.disableSearch)
 			) {
 				stateMethods.setDisableSearch();
 			}
@@ -52,7 +59,12 @@ function evaluateFeatures(avaliableFeatures, stateMethods, states) {
 	}
 }
 
-async function changeServers(server, stateMethods, states, showMessage) {
+async function changeServers(
+	server,
+	stateMethods,
+	stateVariables,
+	showMessage
+) {
 	const baseUrl = "https://localhost:";
 	const newUrl = baseUrl + server.extension;
 	const response = await sendAPIRequest({ requestType: "config" }, newUrl);
@@ -63,7 +75,7 @@ async function changeServers(server, stateMethods, states, showMessage) {
 		evaluateFeatures(
 			response.features,
 			{ setDisableSearch: stateMethods.setDisableSearch },
-			{ disableSearch: states.disableSearch }
+			{ disableSearch: stateVariables.disableSearch }
 		);
 		stateMethods.toggleFeaturesChecklist();
 	} else {
@@ -108,14 +120,8 @@ function ServerInformation(props) {
 										onClick={() =>
 											changeServers(
 												server,
-												{
-													toggleFeaturesChecklist: props.toggleFeaturesChecklist,
-													setFeaturesRecieved: props.setFeaturesRecieved,
-													setDisableSearch: props.setDisableSearch,
-													processServerConfigSuccess:
-														props.processServerConfigSuccess,
-												},
-												{ disableSearch: props.disableSearch },
+												props.stateMethods,
+												props.stateVariables,
 												props.showMessage
 											)
 										}
@@ -127,13 +133,16 @@ function ServerInformation(props) {
 						</Dropdown>
 					</div>
 					<br />
-					<a className="tco-text" onClick={props.toggleFeaturesChecklist}>
+					<a
+						className="tco-text"
+						onClick={props.stateMethods.toggleFeaturesChecklist}
+					>
 						({props.serverSettings.serverUrl}).
 					</a>
 					<FeaturesChecklist
-						isOpen={props.featuresChecklistOpen}
-						toggleOpen={props.toggleFeaturesChecklist}
-						features={props.featuresRecieved}
+						isOpen={props.stateVariables.featuresChecklistOpen}
+						toggleOpen={props.stateMethods.toggleFeaturesChecklist}
+						features={props.stateVariables.featuresRecieved}
 						supportedFeatures={supportedFeatures}
 					/>
 				</div>
