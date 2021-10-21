3008e87d2de28492dc1a02a1ca8bcf9d2dc668f5
bilprice <bilprice@colostate.edu>
2021-10-21 00:29:44 +0000

    Updated footer tests for buttons

--- client/test/components/Trip/TripToolbox.test.js
+++ client/test/components/Trip/TripToolbox.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { beforeEach, describe, expect, it, jest } from '@jest/globals';
 import TripToolbox from '../../../src/components/Trip/Itinerary/TripToolbox';
+import Button from '../../../src/components/Trip/Itinerary/TripToolbox';
 import { render,screen } from '@testing-library/react';
+
 
 describe('Trip Toolbox Modal', ()=>{
     const toggleOpen = jest.fn();
@@ -23,4 +25,24 @@ describe('Trip Toolbox Modal', ()=>{
         const upload = screen.getByTestId('upload-btn');
         expect(upload).toBeTruthy();
     });
+
+    it('has a CSV download button', ()=>{
+        const download = screen.getByTestId('CSV-download-button');
+        expect(download).toBeTruthy();
+    });
+
+    it('has a JSON download button', ()=>{
+        const download = screen.getByTestId('JSON-download-button');
+        expect(download).toBeTruthy();
+    });
+
+    it('has a continue button', ()=>{
+        const cont = screen.getByTestId('continue-button');
+        expect(cont).toBeTruthy();
+        
+    });
+
+    it('should call storeJSON on button click', () => {
+        
+    })
 });
