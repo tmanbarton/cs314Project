d0ce1ff0b28f3956acb333f1aa716ba6a6935a38
amir-raissi <amir_raissi@yahoo.com>
2021-10-27 16:10:18 -0600

    Created tripactions.js

--- client/src/components/Trip/Itinerary/TripActions.js
+++ client/src/components/Trip/Itinerary/TripActions.js
@@ -1,7 +1,28 @@
 import  React from "react";
+import { ButtonGroup, DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown } from 'reactstrap';
+import { BiDotsVerticalRounded } from 'react-icons/bi';
 
 export default function TripActions(props){
     return (
-        <p>Here</p>
+        <ActionsDropdown>
+            <DropdownItem>
+                <p>BUTTONS GO HERE</p>
+            </DropdownItem>
+        </ActionsDropdown>
+    );
+}
+
+function ActionsDropdown(props) {
+    return (
+        <UncontrolledDropdown direction="left">
+            <DropdownToggle tag="div">
+                <BiDotsVerticalRounded size="1.5em" />
+            </DropdownToggle>
+            <DropdownMenu>
+                <ButtonGroup>
+                    {props.children}
+                </ButtonGroup>
+            </DropdownMenu>
+        </UncontrolledDropdown>
     );
 }
