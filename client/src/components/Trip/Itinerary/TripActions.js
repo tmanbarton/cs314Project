86318797ef7917c09bef89e9a484d92814663b6d
William Price <bilprice@colostate.edu>
2021-10-29 22:52:19 +0000

    added functionality to reversTrip button

--- client/src/components/Trip/Itinerary/TripActions.js
+++ client/src/components/Trip/Itinerary/TripActions.js
@@ -27,7 +27,7 @@ export default function TripActions(props){
                         <ImShuffle size = {24}/>
                     </DropdownItem>
                     <DropdownItem>
-                        <FaAngleDoubleLeft size = {24} />
+                        <FaAngleDoubleLeft onClick={()=> reversePlaces(props.places, {bulkAppend: props.bulkAppend})}  size = {24} />
                     </DropdownItem>
                 </ActionsDropdown>
             </Collapse>
@@ -86,6 +86,8 @@ async function sendTourRequest(request, apiObject, setChangedTrip){
     }    
 }
 
-function reversePlaces(places) {
-	return places.reverse();
+function reversePlaces(places, append) {
+    places.reverse();
+    console.log(places);
+    append.bulkAppend(places);
 }
