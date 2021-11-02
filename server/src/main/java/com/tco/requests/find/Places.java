a33eefd8873019ea908e13ef7e58b3a1da573d90
Tyler Barton <tmanbarton@gmail.com>
2021-11-02 02:48:24 +0000

    remove main from places.java

--- server/src/main/java/com/tco/requests/find/Places.java
+++ server/src/main/java/com/tco/requests/find/Places.java
@@ -8,11 +8,4 @@ class Places extends ArrayList<Place> {
         }
     }
     public Places() {}
-
-    public static void main(String[] args) {
-        Places places = new Places();
-        place1 = new Place();
-        place1.put("a", "b");
-        places.add(place1);
-    }
 }
