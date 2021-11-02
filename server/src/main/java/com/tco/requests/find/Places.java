ab9136c5d71cb5366a3a573a236fbb37f354cc06
Tyler Barton <tmanbarton@gmail.com>
2021-11-02 02:29:21 +0000

    add copy constructors

--- server/src/main/java/com/tco/requests/find/Places.java
+++ server/src/main/java/com/tco/requests/find/Places.java
@@ -1,4 +1,18 @@
 package com.tco.requests;
 import java.util.ArrayList;
 
-class Places extends ArrayList<Place> {}
+class Places extends ArrayList<Place> {
+    public Places(Places places) {
+        for(Place place : places) {
+            this.add(new Place(place));
+        }
+    }
+    public Places() {}
+
+    public static void main(String[] args) {
+        Places places = new Places();
+        place1 = new Place();
+        place1.put("a", "b");
+        places.add(place1);
+    }
+}
