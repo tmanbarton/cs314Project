1d221893e6a8e8cfe54c2c4dde330b3beeefa72f
Tyler Barton <tmanbarton@gmail.com>
2021-11-02 03:14:09 +0000

    Actual hash map method

--- server/src/main/java/com/tco/requests/find/Place.java
+++ server/src/main/java/com/tco/requests/find/Place.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 public class Place extends HashMap<String, String> {
     public Place(Place place) {
-        for(String key : place.getKeys()) {
+        for(String key : place.keySet()) {
             this.put(key, place.get(key));
         }
     }
