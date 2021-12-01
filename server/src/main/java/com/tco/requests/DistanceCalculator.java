2d7f20761667c2415409feaa377a99b4100a6c7e
Sonu Dileep <59635031+SonuDileep@users.noreply.github.com>
2021-12-01 10:27:38 -0700

    Update DistanceCalculator.java

--- server/src/main/java/com/tco/requests/DistanceCalculator.java
+++ server/src/main/java/com/tco/requests/DistanceCalculator.java
@@ -4,10 +4,10 @@ import java.lang.Math;
 import java.util.ArrayList;
 
 public class DistanceCalculator {
-    private Places places;
+    private Place[] places;
     private double earthRadius; 
 
-    public DistanceCalculator(Places places, double earthRadius){
+    public DistanceCalculator(Place[] places, double earthRadius){
         this.places = places;
         this.earthRadius = earthRadius;
     }
@@ -16,30 +16,30 @@ public class DistanceCalculator {
         return buildDistancesArray(places);
     }
 
-    public ArrayList<Long> buildDistancesArray(Places places) {
+    public ArrayList<Long> buildDistancesArray(Place[] places) {
         ArrayList<Long> distances = new ArrayList<Long>();
         double latitude1 = 0;
         double longitude1 = 0;
         double latitude2 = 0;
         double longitude2 = 0;
-        for (int i = 1; i < places.size(); i++) {
-          latitude1 = Double.parseDouble(places.get(i - 1).get("latitude"));
-          longitude1 = Double.parseDouble(places.get(i - 1).get("longitude"));
+        for (int i = 1; i < places.length; i++) {
+          latitude1 = Double.parseDouble(places[i - 1].get("latitude"));
+          longitude1 = Double.parseDouble(places[i - 1].get("longitude"));
     
-          latitude2 = Double.parseDouble(places.get(i).get("latitude"));
-          longitude2 = Double.parseDouble(places.get(i).get("longitude"));
+          latitude2 = Double.parseDouble(places[i].get("latitude"));
+          longitude2 = Double.parseDouble(places[i].get("longitude"));
     
           Long distance =
               computeDistance(latitude1, latitude2, longitude1, longitude2);
           distances.add(distance);
         }
         latitude1 =
-            Double.parseDouble(places.get(places.size() - 1).get("latitude"));
+            Double.parseDouble(places[places.length - 1].get("latitude"));
         longitude1 =
-            Double.parseDouble(places.get(places.size() - 1).get("longitude"));
+            Double.parseDouble(places[places.length - 1].get("longitude"));
     
-        latitude2 = Double.parseDouble(places.get(0).get("latitude"));
-        longitude2 = Double.parseDouble(places.get(0).get("longitude"));
+        latitude2 = Double.parseDouble(places[0].get("latitude"));
+        longitude2 = Double.parseDouble(places[0].get("longitude"));
     
         Long distance =
             computeDistance(latitude1, latitude2, longitude1, longitude2);
