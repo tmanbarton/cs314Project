4c0985aefd95399bb776da49df50a990534130d1
Tyler Barton <tmanbarton@gmail.com>
2021-12-07 18:11:14 +0000

    Add descriptions to modal

--- client/src/components/Margins/FeaturesChecklist.js
+++ client/src/components/Margins/FeaturesChecklist.js
@@ -34,37 +34,52 @@ function Header() {
 }
 
 function Body(props) {
-	const findDescription = "Select the search icon to search for a place and choose to switch from searching by name to searching by coordinates with the globe icon. You can also choose to get random places with the dice icon.";
-	const distancesDescription = "";
-	const tourDescription = "";
+	const findDescription = "Select the search icon to search for a place by name. Use the globe icon to switch to searching by coordinates and the dice icon to generate random places.";
+	const distancesDescription = "The total distance of your trip is shown at the top and bottom of your itinerary. The cumulative distance is shown when you expand the individual locations.";
+	const tourDescription = "You can edit your trip by dragging and dropping with the drag handles, or with the options in the \"Modify\" menu: optimize your trip to get a shorter route, randomly order the trip, reverse the trip, or remove all of the locations.";
+
 	return (
 		<ModalBody className="center-modal-body">
 			{props.supportedFeatures.map((feature, index) => (
 				<div>
-					<Row className="centered" key={`${index} - ${feature}`}>
-						<Col>
-							<h2
-								style={
-									evaluateSupport(props.features, feature)
-										? { color: "#2e8540" }
-										: { color: "#cd2026" }
-								}
-							>
-								{feature}
-							</h2>
-						</Col>
-						<Col>
-							{evaluateSupport(props.features, feature) ? (
-								<FaCheckSquare style={{ color: "#2e8540" }} size={32} />
-							) : (
-								<FaWindowClose style={{ color: "#cd2026" }} size={32} />
-							)}
-						</Col>
-						<hr />
-					</Row>
-					<Row>
-
-					</Row>
+					{ feature != "config" &&
+						<Row className="centered" key={`${index} - ${feature}`}>
+							<Col>
+								<h4
+									style={
+										evaluateSupport(props.features, feature)
+											? { color: "#2e8540" }
+											: { color: "#cd2026" }
+									}
+								>
+									{feature}
+								</h4>
+							</Col>
+							<Col>
+								{evaluateSupport(props.features, feature) ? (
+									<FaCheckSquare style={{ color: "#2e8540" }} size={32} />
+								) : (
+									<FaWindowClose style={{ color: "#cd2026" }} size={32} />
+								)}
+							</Col>
+							<hr />
+						</Row>
+					}
+					{ feature == "find" &&
+						<Row style={{ textAlign: "left" }}>
+							{findDescription}
+						</Row>
+					}
+					{ feature == "distances" &&
+						<Row style={{ textAlign: "left" }}>
+							{distancesDescription}
+						</Row>
+					}
+					{ feature == "tour" &&
+						<Row style={{ textAlign: "left" }}>
+							{tourDescription}
+						</Row>
+					}
 				</div>
 			))}
 		</ModalBody>
